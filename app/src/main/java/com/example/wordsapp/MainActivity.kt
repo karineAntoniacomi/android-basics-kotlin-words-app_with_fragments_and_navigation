f6701c7063c4f8dea3c7eb645c9fea518799b49f
Karine Antoniacomi <karineas@pbtech.net.br>
2022-11-17 16:30:43 -0300

    adiciona demais arquivos do projeto

---
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.wordsapp.databinding.ActivityMainBinding
+
+/**
+ * Main Activity and entry point for the app.
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // acessa referência ao nav_host_fragment (o ID da FragmentContainerView)
+        // e atribui à sua propriedade navController
+        // Get the navigation host fragment from this Activity
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // garante que os botões da barra de ações como a opção de menu
+        // no LetterListFragment, sejam visíveis
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Enables back button support. Simply navigates one element up on the stack.
+     */
+    // Define o defaultNavHost como true no XML e permite que processe o botão Para
+    // cima, a atividade precisa fornecer a implementação
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
